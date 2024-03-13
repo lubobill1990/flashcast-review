2ee86552f7acd0073cc09c597cc269f9f85ac0e4
Yinran Li <yinranli@microsoft.com>
2024-03-13 11:50:40 +0800

    test blob storage.

--- apps/portal/src/app/sample/create/actions.ts
+++ apps/portal/src/app/sample/create/actions.ts
@@ -2,14 +2,38 @@
 
 import factory from "@/factory";
 
-export async function submit(_currentState: unknown, formData: FormData) { 
+import { BlobServiceClient, StorageSharedKeyCredential, newPipeline } from '@azure/storage-blob';
+const account = ''; // replace with your storage account name
+const accountKey = ''; // replace with your storage account key
+const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+const pipeline = newPipeline(sharedKeyCredential);
+const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net`, pipeline);
+const containerName = ''; // replace with your container name
 
-  const data = {
-    recording: formData.get("recording") as File,
-    transcription: formData.get("transcription") as File,
-    notes: formData.get("notes") as string,
-  };
+const uploadToBlobStorage = async (file: File): Promise<string> => {
+  const blob = new Blob([file], { type: file.type }); 
+  const blobName = Date.now() + file.name;
+  console.log("uploadToBlobStorage", blobName, blob);
+
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  console.log(containerClient.exists());
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  // fs.readFile(file);
+  await blockBlobClient.uploadData(blob);
+  return `https://${account}.blob.core.windows.net/${containerName}/${blobName}`;
+};
+
+export async function submit(formData: FormData) { 
+// export async function submit(_currentState: unknown, formData: FormData) { 
+  const file = formData.get("recording") as File;
+  const data = await uploadToBlobStorage(file);
+  // const data = {
+  //   recording: formData.get("recording") as File,
+  //   transcription: formData.get("transcription") as File,
+  //   notes: formData.get("notes") as string,
+  // };
 
   console.log(data);
-  return factory.sampleService.createSample(data);
+  // return factory.sampleService.createSample(data);
 }
--- apps/portal/src/app/sample/create/page.tsx
+++ apps/portal/src/app/sample/create/page.tsx
@@ -8,48 +8,50 @@ import { useMemo } from 'react';
 import { submit } from './actions';
 import { useFormState } from 'react-dom';
 
-class Sample {
-  transcription_file: string | null = null;
-  video_file: string | null = null;
-  ai_note: string = '';
+// class Sample {
+//   transcription_file: string = "";
+//   video_file: string = "";
+//   ai_note: string = '';
 
-  constructor() {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
+//   constructor() {
+//     makeAutoObservable(this, {}, { autoBind: true });
+//   }
 
-  setTranscriptionFile(file: string) {
-    this.transcription_file = file;
-  }
+//   setTranscriptionFile(file: string) {
+//     this.transcription_file = file;
+//   }
 
-  setVideoFile(file: string) {
-    this.video_file = file;
-  }
+//   setVideoFile(file: string) {
+//     console.log(file);
+//     this.video_file = file;
+//   }
 
-  setAiNote(note: string) {
-    this.ai_note = note;
-  }
-}
+//   setAiNote(note: string) {
+//     this.ai_note = note;
+//   }
+// }
 
 export default function Page() {
-  const [errorMessage, dispatch] = useFormState(submit, undefined);
-  const sample = useMemo(() => new Sample(), []);
+  // const [errorMessage, dispatch] = useFormState(submit, undefined);
+  // const sample = useMemo(() => new Sample(), []);
 
   return (
     <div>
       <h1>Create a sample</h1>
-      <form action={dispatch}>
+      <form action={submit}>
         <Text as='label'>
           Upload the video recording
-          <input type='file' name="recording" onChange={e => sample.setVideoFile(e.target.value)} />
+          <input type='file' name="recording" />
+          {/* <input type='file' name="recording" value={sample.video_file} onChange={e => sample.setVideoFile(e.target.value)} /> */}
         </Text>
-        <Text as='label'>
+        {/* <Text as='label'>
           Upload the transcription
           <textarea name="transcription" onChange={(e) => sample.setTranscriptionFile} />
         </Text>
         <Text as='label'>
           AI Note
           <textarea name="notes" onChange={(e) => sample.setAiNote} />
-        </Text>
+        </Text> */}
 
         <Button>Create</Button>
       </form>
